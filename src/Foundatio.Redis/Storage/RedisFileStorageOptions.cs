55e336d5ff4cd91423e2c5625bdf31d51d958cff
Edward Meng <edward.meng@gaiaworks.cn>
2018-03-07 14:10:18 +0800

    Add implementation for file storage in redis.

---
+++ src/Foundatio.Redis/Storage/RedisFileStorageOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Foundatio.Storage {
+    public class RedisFileStorageOptions : SharedOptions {
+        public ConnectionMultiplexer ConnectionMultiplexer { get; set; }
+        public string ContainerName { get; set; } = "storage";
+    }
+
+    public class RedisFileStorageOptionsBuilder : SharedOptionsBuilder<RedisFileStorageOptions, RedisFileStorageOptionsBuilder> {
+        public RedisFileStorageOptionsBuilder ConnectionMultiplexer(ConnectionMultiplexer connectionMultiplexer) {
+            Target.ConnectionMultiplexer = connectionMultiplexer;
+            return this;
+        }
+
+        public RedisFileStorageOptionsBuilder ContainerName(string containerName) {
+            Target.ContainerName = containerName ?? throw new ArgumentNullException(nameof(containerName));
+            return this;
+        }
+    }
+}
