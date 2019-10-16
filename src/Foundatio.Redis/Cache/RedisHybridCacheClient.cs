494ae21c27ea91f3df9e61ede428cc71f2fa7d0f
foresightyj <imyuanjian@gmail.com>
2019-10-16 13:31:57 +0800

    added redisChannelName for RedisHybridCacheClient so that users can supply their preferred redis pubsub channel name for cache synchronization

--- src/Foundatio.Redis/Cache/RedisHybridCacheClient.cs
+++ src/Foundatio.Redis/Cache/RedisHybridCacheClient.cs
@@ -6,19 +6,19 @@ using StackExchange.Redis;
 
 namespace Foundatio.Caching {
     public class RedisHybridCacheClient : HybridCacheClient {
-        public RedisHybridCacheClient(RedisCacheClientOptions options, InMemoryCacheClientOptions localOptions = null)
+        public RedisHybridCacheClient(RedisCacheClientOptions options, InMemoryCacheClientOptions localOptions = null, string redisChannelName = "cache-messages")
             : base(new RedisCacheClient(o => o
                 .ConnectionMultiplexer(options.ConnectionMultiplexer)
                 .Serializer(options.Serializer)
                 .LoggerFactory(options.LoggerFactory)),
             new RedisMessageBus(o => o
                 .Subscriber(options.ConnectionMultiplexer.GetSubscriber())
-                .Topic("cache-messages")
+                .Topic(redisChannelName)
                 .Serializer(options.Serializer)
                 .LoggerFactory(options.LoggerFactory)), localOptions, options.LoggerFactory) { }
 
-        public RedisHybridCacheClient(Builder<RedisCacheClientOptionsBuilder, RedisCacheClientOptions> config, Builder<InMemoryCacheClientOptionsBuilder, InMemoryCacheClientOptions> localConfig = null)
-            : this(config(new RedisCacheClientOptionsBuilder()).Build(), localConfig(new InMemoryCacheClientOptionsBuilder()).Build()) { }
+        public RedisHybridCacheClient(Builder<RedisCacheClientOptionsBuilder, RedisCacheClientOptions> config, Builder<InMemoryCacheClientOptionsBuilder, InMemoryCacheClientOptions> localConfig = null, string redisChannelName = "cache-messages")
+            : this(config(new RedisCacheClientOptionsBuilder()).Build(), localConfig(new InMemoryCacheClientOptionsBuilder()).Build(), redisChannelName) { }
 
         public override void Dispose() {
             base.Dispose();
