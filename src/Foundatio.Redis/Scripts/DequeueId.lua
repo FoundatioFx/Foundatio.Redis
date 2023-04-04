4cd403b32f3661f8dac920d20c2906ad272ed8b0
Mathew Heard <splitice@users.noreply.github.com>
2023-04-05 02:14:01 +1000

    redis cluster support (#77)

--- src/Foundatio.Redis/Scripts/DequeueId.lua
+++ src/Foundatio.Redis/Scripts/DequeueId.lua
@@ -1,7 +1,7 @@
 ﻿local item = redis.call('RPOPLPUSH', @queueListName, @workListName);
 if item then
-    local dequeuedTimeKey = 'q:' .. @queueName .. ':' .. item .. ':dequeued';
-    local renewedTimeKey = 'q:' .. @queueName .. ':' .. item .. ':renewed';
+    local dequeuedTimeKey = @listPrefix .. ':' .. item .. ':dequeued';
+    local renewedTimeKey = @listPrefix .. ':' .. item .. ':renewed';
     redis.call('SET', dequeuedTimeKey, @now, 'PX', @timeout);
     redis.call('SET', renewedTimeKey, @now, 'PX', @timeout);
     return item;
