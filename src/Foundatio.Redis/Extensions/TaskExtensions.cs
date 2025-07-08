a041fe99bc09a2d54ac307dd6cdb245d4dde2a25
Blake Niemyjski <bniemyjski@gmail.com>
2025-07-07 22:22:16 -0500

    Optimizes task awaiting using aggressive inlining
    Improves performance by using aggressive inlining for the AnyContext extension methods. This reduces overhead associated with method calls, especially in hot paths.

--- src/Foundatio.Redis/Extensions/TaskExtensions.cs
+++ src/Foundatio.Redis/Extensions/TaskExtensions.cs
@@ -9,18 +9,21 @@ namespace Foundatio.Extensions;
 internal static class TaskExtensions
 {
     [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable<TResult> AnyContext<TResult>(this Task<TResult> task)
     {
         return task.ConfigureAwait(continueOnCapturedContext: false);
     }
 
     [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable AnyContext(this Task task)
     {
         return task.ConfigureAwait(continueOnCapturedContext: false);
     }
 
     [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ConfiguredTaskAwaitable<TResult> AnyContext<TResult>(this AwaitableDisposable<TResult> task) where TResult : IDisposable
     {
         return task.ConfigureAwait(continueOnCapturedContext: false);
